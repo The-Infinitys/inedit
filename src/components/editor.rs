665a5c3fb3914d57c9ba93116907e63002f71efd
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2025-06-22 14:56:56 +0000

    odified

--- src/components/editor.rs
+++ src/components/editor.rs
@@ -1,5 +1,5 @@
 use ratatui::widgets::{Block, Borders, Paragraph};
 // Editorコンポーネント
 pub fn editor<'a>() -> Paragraph<'a> {
-    Paragraph::new("ここがメインスペースです。").block(Block::default().borders(Borders::ALL))
+    Paragraph::new("ここがメインスペースです。").block(Block::default().borders(Borders::NONE))
 }
