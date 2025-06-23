5191f2097d43819d066d38f5b92972409f906b6f
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-06-23 10:56:30 +0900

    add some items

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -5,7 +5,7 @@ pub fn handle_events(app: &mut App) -> io::Result<()> {
     if event::poll(std::time::Duration::from_millis(100))? {
         if let Event::Key(key) = event::read()? {
             match key.code {
-                KeyCode::Char('q') => app.should_quit = true,
+                KeyCode::Char('w') => app.should_quit = true,
                 _ => {}
             }
         }
